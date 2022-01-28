b907ea2e4fa5b99a3b58e826d5ee76b085037fa4
Cristian Fernandes <cristian.daniel.fernandes@gmail.com>
2022-01-28 17:56:54 -0300

    Atualizaçao das informaçoes

--- curriculo-expresso/app.js
+++ curriculo-expresso/app.js
@@ -16,24 +16,24 @@ app.get('/', function (req, res, next) {
     app.get('/curriculo',(req, res, next) => {
         res.render('curriculo', {
             title: 'Meu currículo',
-            name: 'William Oliveira',
-            profession: 'Software Engineer',
-            description: 'Experiência em desenvolvimento de single page applications com JavaScript e frameworks JavaScript (já trabalhei com Angular e React), module bundlers, package managers, transpilers (como Babel), pre processadores CSS (Sass e Stylus), task managers, arquitetura CSS (como BEM e SMACSS), Git, SEO, acessibilidade e usabilidade.',
+            name: 'Cristian Fernandes',
+            profession: 'TI',
+            description: 'Desc',
             experience: [{
-                company: 'Loggi Tecnologia',
+                company: 'LoTecnologia',
                 office: 'Software Engineer',
                 description: 'Trabalho no squad de desenvolvimento do software de gestão de warehouses da Loggi, o ProXD. Nosso trabalho é automatizar os processos de recebimento, gestão, armazenamento, transferências e expedição de pacotes, cortes, rotas e monitoramento dos pacotes e sacas de entregas para ecommerces.'
             },
             {
                 company: 'Casa do Código',
-                office: 'Escritor',
-                description: 'Autor do livro: O universo da programação: Um guia de carreira em desenvolvimento de software'
+                office: 'Des',
+                description: 'Desenvolvimento de software'
             }],
             education: [{
-                institution: 'Vida',
-                description: 'Sobrevivência nas ruas'
+                institution: 'Digital House',
+                description: 'CTD - Certified Tech Developer'
             }],
-            skills: ['backend', 'frontend', 'infra', 'mobile']
+            skills: ['Full Stack']
         });
     });
 });
