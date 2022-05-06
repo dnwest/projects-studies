7ebce87504a478a21177eee3b41dce7af0c7a2ee
dnwest <cristian.daniel.fernandes@gmail.com>
2022-05-05 22:14:18 -0300

    Create Feedback and Styles

--- nlwReturn/web/src/components/WidgetForm/index.tsx
+++ nlwReturn/web/src/components/WidgetForm/index.tsx
@@ -1,8 +1,9 @@
-import { CloseButton } from "./CloseButton";
+import { useState } from "react";
+import { CloseButton } from "../CloseButton";
 
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
 
 const feedbackTypes = {
     BUG: {
@@ -28,7 +29,11 @@ const feedbackTypes = {
         },
 };
 
+type FeedbackType = keyof typeof feedbackTypes;
+
 export function WidgetForm() {
+    const [feedbackType, setFeedBackType] = useState<FeedbackType | null>(null)
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             <header>
@@ -36,11 +41,15 @@ export function WidgetForm() {
                 <CloseButton />
             </header>
 
+        {!feedbackType ? (
             <div className="flex py-8 gap-2 w-full">
                 {Object.entries(feedbackTypes).map(([key, value]) => {
                     return (
                         <button
-                        className=""
+                        key={key}
+                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
+                        onClick={() => setFeedBackType(key as FeedbackType)}
+                        type="button"
                         >
                             <img src={value.image.source} alt={value.image.alt} />
                             <span>{value.title}</span>
@@ -48,7 +57,9 @@ export function WidgetForm() {
                     );
                 })}
             </div>
-
+) :(
+    <p>Hello World</p>
+)}
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ pela  <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
             </footer>
